80cdd0116d128c60f0c8b7566710d32c5e12e224
Elisaveta Protsenko <elisaveta.pro@gmail.com>
2016-05-18 17:28:44 +0300

    new file for nested lists of data

--- BROM-pics.py
+++ BROM-pics.py
@@ -6,10 +6,10 @@ value=[]
 f = open('output_240_day-april.dat', 'rb')  #open model output file
 
 num_lines = sum(1 for l  in f)
-num = num_lines - 3  # calculate number of lines 
+num = num_lines - 4  # calculate number of lines 
 f.seek(0)                      # return to the beginning of the file 
 
-for _ in range(2):            # skip two unneeded lines 
+for _ in range(1):            # skip two unneeded lines 
     line = f.readline()
     
 line = f.readline()           # read line for heading
@@ -23,38 +23,30 @@ loo = line.split()
 item = 0
 for k, v in d.iteritems(): 
     v = loo[item]
-    value.append(loo[item])
     item += 1
-    d[k] = v
-                    
-#for n in range(num):
-line = f.readline()
-loo = line.split()
-item = 0
-#for item in loo:
+    d[k] = [v]
+ 
+               
+for n in range(num):   # add all lines values to dict
+    item = 0
+    line = f.readline()
+    koo = line.split()
+    for k, v in d.iteritems(): 
+        v.append(koo[item])
+        item += 1
 
-for k, v in d.iteritems(): 
-#        value.append(loo[item])
-    item += 1
- #       v = value
-    d[k] = v
-#
 #    for i in range(42):
 #        [v].append(loo[1])
- 
 
+
+    
 #print d
-print d
-
-
-
-
 #Draw model results
 
 plt.figure(1)
 
 plt.subplot(311)
-plt.title('Water')            # subplot 313 title
+plt.title('Water')            # subplot Temperature title
 plt.xlabel('$\mu$M') 
 plt.ylabel('M')
 plt.axis([0, 160, 0, 110])
@@ -71,3 +63,14 @@ plt.ylabel('Cm')
 
 
 #plt.show()
+
+
+
+plt.figure(2)
+plt.subplot()
+plt.title('Water')            # subplot Temperature title
+plt.xlabel('$\mu$M') 
+plt.ylabel('M')
+plt.axis([0, 160, 0, 110])
+#plt(d['Depth'],['t'])
+plt.show()
